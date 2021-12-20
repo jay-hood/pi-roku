b1d1c8bfecccc50a2d9e9ce58f5973fe0403c1ec
Jay Hood <hood.jay@gmail.com>
2021-12-20 09:16:20 -0500

    layout figured out

---
+++ main.py
@@ -0,0 +1,42 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.anchorlayout import AnchorLayout
+
+from kivy.uix.button import Button
+from kivy.uix.scrollview import ScrollView
+
+
+class ContainerBox(BoxLayout):
+    pass
+
+
+class VBoxWidget(Widget):
+    pass
+
+
+class HBoxWidget(Widget):
+    pass
+
+
+class Scroll(ScrollView):
+    pass
+
+
+class Header(AnchorLayout):
+    pass
+
+
+class Combined(BoxLayout):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return Combined()
+        # return ContainerBox()
+
+
+MainApp().run()
